0d1ec53952ed46efc7f9bb3e59b05b0ffdf04054
Sherif Ahmed <sherif201665@gmail.com>
2024-08-04 20:15:16 +0000

    ball analysis

--- tracker/BallTracker.py
+++ tracker/BallTracker.py
@@ -10,11 +10,11 @@ class BallTracker:
     def interpolate_ball_positions(self, ball_positions):
         ball_positions = [x.get(1,[]) for x in ball_positions]
         # convert the list into pandas dataframe
-        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+        df_ball_positions = pd.DataFrame(ball_positions, columns=['x1','y1','x2','y2'])
 
         # interpolate the missing values
-        df_ball_positions = df_ball_positions.interpolate()
-        df_ball_positions = df_ball_positions.bfill()
+        df_ball_positions = df_ball_positions.interpolate() 
+        df_ball_positions = df_ball_positions.bfill() # Fills NaNs with the next valid observation
 
         ball_positions = [{1:x} for x in df_ball_positions.to_numpy().tolist()]
 
